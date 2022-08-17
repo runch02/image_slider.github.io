2ee67291129ab7946924e29355768be793ce30d7
Your Name <runch762@gmail.com>
2022-08-17 20:12:07 +0530

    image-slider

---
+++ image.js
@@ -0,0 +1,24 @@
+let imageData = [
+    "https://cdn.pixabay.com/photo/2022/07/30/14/07/butterfly-7353884_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2022/02/07/12/17/street-6999284_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/07/17/15/03/birds-7327611_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/07/31/17/07/bird-7356346_1280.jpg",
+    "https://cdn.pixabay.com/photo/2020/10/14/20/31/hands-5655424_1280.png",
+    "https://cdn.pixabay.com/photo/2022/07/25/15/39/mum-7344070_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/12/16/23/thuja-7382049_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/07/22/18/50/helenium-7338764_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/07/26/03/35/jogger-7344979_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/09/19/55/boho-art-7375748_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/13/18/59/bark-7384291_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/03/16/26/prairie-7362991_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/10/07/25/church-7376515_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/06/15/05/31/sparrow-7263192_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/05/09/18/05/dog-7185274_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/01/06/08/irbene-7357426_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/08/12/12/25/vulture-7381642_1280.jpg",
+    "https://cdn.pixabay.com/photo/2021/11/28/15/30/cathedral-6830526_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/06/27/14/38/cat-7287671_1280.jpg",
+    "https://cdn.pixabay.com/photo/2022/07/24/17/55/wind-energy-7342177_1280.jpg",
+]
+
+export { imageData }
---
+++ script.js
@@ -0,0 +1,38 @@
+import { imageData } from "./image.js"
+
+class ImageSlider {
+    constructor() {
+        this.initialImageNumber = 0
+        this.setRandomImageInitially()
+    }
+
+    setRandomImageInitially() {
+        this.randomNumber = Math.floor(Math.random() * imageData.length)
+        document.querySelector(".image-container img").src = imageData[this.randomNumber]
+        this.initialImageNumber = this.randomNumber
+    }
+
+    leftArrowImage() {
+        if (this.initialImageNumber <= 0) {
+            this.initialImageNumber = imageData.length - 1
+        } 
+        
+        document.querySelector(".image-container img").src = imageData[this.initialImageNumber -= 1]
+        console.log(this.initialImageNumber)
+    }
+    
+    rightArrowImage() {
+        if (this.initialImageNumber >= imageData.length - 1) {
+            this.initialImageNumber = 0
+        } 
+
+        document.querySelector(".image-container img").src = imageData[this.initialImageNumber += 1]
+        console.log(this.initialImageNumber)
+    }
+}
+
+let imageSliderFirstInstance = new ImageSlider()
+
+document.querySelector(".left-arrow").addEventListener("click", () => imageSliderFirstInstance.leftArrowImage())
+
+document.querySelector(".right-arrow").addEventListener("click", () => imageSliderFirstInstance.rightArrowImage())
